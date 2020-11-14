48417bda2ed8bbefac9c2f9b4aaca94c2e08bdea
pagecow <pagecow@gmail.com>
2020-11-14 14:05:11 -0600

    mvp finished

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+    "name": "tj-mccarty-portfolio-official",
+    "version": "1.0.0",
+    "description": "",
+    "scripts": {
+        "start": "node server.js"
+    },
+    "dependencies": {
+        "express": "^4.17.1"
+    }
+}
