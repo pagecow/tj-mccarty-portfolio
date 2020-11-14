b4b6754d26564f2e19fa69cbebff4c7546b56938
pagecow <pagecow@gmail.com>
2020-11-14 12:44:47 -0600

    updated server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
-const express = require("express"),
-  app = express(),
-  cors = require("cors"),
-  SERVER_PORT = "8080";
+var express = require('express');
+var app = express();
+var path = require('path');
 
-app.use(express.json());
-app.use(cors());
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-app.use('/', express.static('app', {index: "index.html"}));
+app.listen(8080);
 
-// app.use( express.static( `${__dirname}/../index.html` ) );
 
-app.listen(SERVER_PORT, () => console.log(`Server is running on port ${SERVER_PORT}.`));
+
+// const express = require("express"),
+//   app = express(),
+//   cors = require("cors"),
+//   SERVER_PORT = "8080";
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.use('/', express.static('app', {index: "index.html"}));
+
+// // app.use( express.static( `${__dirname}/../index.html` ) );
+
+// app.listen(SERVER_PORT, () => console.log(`Server is running on port ${SERVER_PORT}.`));
